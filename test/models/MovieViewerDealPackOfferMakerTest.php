2cae61c7a9dd510706f235b7a9cc4dffc2e39bbd
Toshiyuki Ando <couger@kt.rim.or.jp>
2015-10-05 23:06:29 +0900

    期限切れ1ヶ月まえ、オファーしないパターンを追加

--- test/models/MovieViewerDealPackOfferMakerTest.php
+++ test/models/MovieViewerDealPackOfferMakerTest.php
@@ -22,7 +22,39 @@ class MovieViewerDealPackOfferMakerTest extends PHPUnit_Framework_TestCase {
         $GLOBALS['movieviewer_settings'] = $settings;
     }
 
-    public function testGetOfferReturns2Offers() {
+    public function testGetOfferNoOffer() {
+        // 視聴期限切れ1ヶ月と1秒まえ
+        $date_freeze = new DateTime("2015-10-14 23:59:59+09:00");
+        timecop_freeze($date_freeze->getTimestamp());
+
+        $user = new MovieViewerUser();
+        $user->id = "aaa@bbb.ccc";
+        $user->selected_courses = array("K1Kiso","K2Kiso");
+        $maker = new MovieViewerDealPackOfferMaker($user);
+        $offers = $maker->getOffers();
+
+        $this->assertEquals(1, count($offers));
+        $this->assertEquals("K2Kiso-1", $offers[0]->getPackId());
+    }
+
+    public function testGetOfferAboutToExpire() {
+        // 視聴期限切れ1ヶ月
+        $date_freeze = new DateTime("2015-10-15 00:00:00+09:00");
+        timecop_freeze($date_freeze->getTimestamp());
+
+        $user = new MovieViewerUser();
+        $user->id = "aaa@bbb.ccc";
+        $user->selected_courses = array("K1Kiso","K2Kiso");
+        $maker = new MovieViewerDealPackOfferMaker($user);
+        $offers = $maker->getOffers();
+
+        $this->assertEquals(2, count($offers));
+        $this->assertEquals("K1Kiso-2", $offers[0]->getPackId());
+        $this->assertEquals("K2Kiso-1", $offers[1]->getPackId());
+    }
+
+    public function testGetOfferAlreadyExpired() {
+        // 11-15で視聴期限が切れる
         $date_freeze = new DateTime("2015-11-15 00:00:01+09:00");
         timecop_freeze($date_freeze->getTimestamp());
 

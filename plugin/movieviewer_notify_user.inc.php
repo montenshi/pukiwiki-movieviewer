a96b41f9e56f7a67d032cd1a3af41d95dcf7a867
Toshiyuki Ando <couger@kt.rim.or.jp>
2015-10-28 17:05:16 +0900

    関数名、変数名を見直し

--- plugin/movieviewer_notify_user.inc.php
+++ plugin/movieviewer_notify_user.inc.php
@@ -23,7 +23,7 @@ function plugin_movieviewer_notify_user_convert(){
     $start_pages['bank']   = $page_args[0];
     $start_pages['credit'] = $page_args[1];
 
-    $purchase_notice = plugin_movieviewer_notify_user_convert_purchase_notice($user, $start_pages);
+    $purchase_offer = plugin_movieviewer_notify_user_convert_purchase_offer($user, $start_pages);
 
     $content =<<<TEXT
     <script src="https://code.jquery.com/jquery-1.11.2.min.js"></script>
@@ -31,13 +31,13 @@ function plugin_movieviewer_notify_user_convert(){
     <link href="https://code.jquery.com/ui/1.11.4/themes/redmond/jquery-ui.css" rel="stylesheet">
     <link href="plugin/movieviewer/movieviewer.css" rel="stylesheet">
     <h2>お知らせ</h2>
-    $purchase_notice
+    $purchase_offer
 TEXT;
 
     return $content;
 }
 
-function plugin_movieviewer_notify_user_convert_purchase_notice($user, $start_pages) {
+function plugin_movieviewer_notify_user_convert_purchase_offer($user, $start_pages) {
 
     $settings = plugin_movieviewer_get_global_settings();
     $offer_maker = new MovieViewerDealPackOfferMaker($settings->payment, $user);
